7b51d99554fcaa3f11111da6071e98c4257901fa
ClassiCcOwl <khavari.7878@yahoo.com>
2024-06-15 10:11:02 +0330

    refactor: seprated settings

--- HamsterKombat/wsgi.py
+++ HamsterKombat/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HamsterKombat.settings')
+# TODO: change into product
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HamsterKombat.settings.local")
 
 application = get_wsgi_application()
