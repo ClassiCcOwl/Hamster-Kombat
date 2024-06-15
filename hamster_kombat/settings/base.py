4a9ed3e7a7b4be6a6a1f510dedaf363cb016a74a
ClassiCcOwl <khavari.7878@yahoo.com>
2024-06-15 10:48:28 +0330

    fix: remove custom logger

--- hamster_kombat/settings/base.py
+++ hamster_kombat/settings/base.py
@@ -122,21 +122,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_URL = "static/"
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
-        }
-    },
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        }
-    },
-    "root": {"level": "INFO", "handlers": ["console"]},
-}
