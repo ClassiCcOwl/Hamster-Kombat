9024b7c1a2fed628a6aa52563f872834f286899d
classiccowl <khavari.7878@yahoo.com>
2024-07-09 14:25:41 +0330

    fix: profile creat signal

--- core_apps/users/apps.py
+++ core_apps/users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core_apps.users"
+
+    def ready(self):
+        # noinspection PyUnresolvedReferences
+        from . import signals  # noqa: F401
--- core_apps/users/signals.py
+++ core_apps/users/signals.py
@@ -1,10 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from core_apps.users.models import User
-from .models import Profile
+from ..profiles.models import Profile
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    print('new user singal')
     if created:
         Profile.objects.create(user=instance)
